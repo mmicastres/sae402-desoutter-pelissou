a1459f463a5027f6677e8f803ee766a756b8f12c
Jules-Pelissou <jules.pelissou@gmail.com>
2023-04-05 09:50:33 +0200

    Ajout Nav

--- reactmusique/src/App.js
+++ reactmusique/src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import MaCard from './components/Card';
+import NavBar from './Views/NavBar';
 
 function App() {
   return (
     <div className="App">
-  
+      <NavBar></NavBar>
       <Button variant="contained">Hello World</Button>
       <MaCard></MaCard>
       
---
+++ reactmusique/src/Views/NavBar.js
@@ -0,0 +1,220 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+
+
+function ResponsiveAppBar() {
+  // === gestion menu de gauche (burger ou liste suivant la taille)
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  // -- afficher le menu à la position de la cible de l'event
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  // -- fermer le menu -> la position est nulle
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+  // === gestion menu de droite (profil user)
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="fixed">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          {/* ==========  icone android à gauche 
+                  s'affiche uniquement si taille écran = md */}
+          <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+          {/* ==========  libellé à gauche qui
+                  s'affiche uniquement si taille écran = md */}
+            <Typography
+              variant="h6"
+              noWrap
+              sx={{
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "inherit",
+                textDecoration: "none"
+              }}
+            >
+              Mon Appli
+            </Typography>
+          {/* menu burger qui s'affiche uniquement si taille écran = md */}
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            {/* icon burger */}
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu} // ouvre/ferme le menu
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            {/* le menu en popup si taille écran < md */}
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav} // position du menu
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left"
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left"
+              }}
+              open={Boolean(anchorElNav)} // affiché / caché en fonction du state
+              onClose={handleCloseNavMenu} // fonction exécutée quand on ferme le menu
+              sx={{
+                display: { xs: "block", md: "none" }
+              }}
+            >
+              <MenuItem onClick={handleCloseNavMenu}>
+
+                  <Typography textAlign="center">Produits</Typography>
+
+              </MenuItem>
+              <MenuItem onClick={handleCloseNavMenu}>
+ 
+                  <Typography textAlign="center">Prix</Typography>
+
+              </MenuItem>
+              <MenuItem onClick={handleCloseNavMenu}>
+
+                  <Typography textAlign="center">Dzerfzuetrfuzeo</Typography>
+
+              </MenuItem>
+            </Menu>
+          </Box>
+          {/* ==========  icone android à gauche 
+                  s'affiche uniquement si taille écran < md */}
+          <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: "flex", md: "none" },
+              flexGrow: 1,
+              fontFamily: "monospace",
+              fontWeight: 700,
+              letterSpacing: ".3rem",
+              color: "inherit",
+              textDecoration: "none"
+            }}
+          >
+            Mon appli
+          </Typography>
+          {/* ==========  menu de gauche mais affiché dans la navbar
+             si la taille de l'écran est assez grande = md */}
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+              <Button
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                Produits
+              </Button>
+
+
+              <Button
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                Prix
+              </Button>
+
+
+              <Button
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "white", display: "block" }}
+              >
+                Dispo
+              </Button>
+
+          </Box>
+          {/* ==========  menu profil user à droite 
+                  toujours identique quelle que soit la taille de l'écran
+          */}
+          <Box sx={{ flexGrow: 0 }}>
+            {/*  -------  icone du user avec tooltip */}
+            <Tooltip title="Open settings">
+              {/* icone pour ouvrir le menu */}
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            {/* le menu avec ses options */}
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right"
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right"
+              }}
+              open={Boolean(anchorElUser)} // position du menu
+              onClose={handleCloseUserMenu} // action si fermeture du menu
+            >
+              <MenuItem onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">Login</Typography>
+              </MenuItem>
+              <MenuItem onClick={handleCloseUserMenu}>
+
+                  <Typography textAlign="center">Ident</Typography>
+
+              </MenuItem>
+              <MenuItem onClick={handleCloseUserMenu}>
+
+                  <Typography textAlign="center">Profile</Typography>
+
+              </MenuItem>
+              <MenuItem onClick={handleCloseUserMenu}>
+   
+                  <Typography textAlign="center">Account</Typography>
+
+              </MenuItem>
+              <MenuItem onClick={handleCloseUserMenu}>
+             
+                  <Typography textAlign="center">Dashboard</Typography>
+
+              </MenuItem>
+              <MenuItem onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">Logout</Typography>
+
+              </MenuItem>
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default ResponsiveAppBar;
