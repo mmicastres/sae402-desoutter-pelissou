6e753edf37c28b7ec4b66357463a597fe8feb016
AlexDesout <you@example.com>
2023-04-11 09:04:50 +0200

    Bouton album aléatoire

--- reactmusique/src/Views/Accueil.js
+++ reactmusique/src/Views/Accueil.js
@@ -1,10 +1,12 @@
 import AlbumAccueil from "../components/AlbumAccueil"
+import { Button } from "@mui/material"
 
 export default function Accueil() {
     return (
         <div className="Accueil">
             {/* <p>Salut</p> */}
             <AlbumAccueil></AlbumAccueil>
+            <Button variant="contained">Album aléatoire</Button>
         </div>
     )
 }
