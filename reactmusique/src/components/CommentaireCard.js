1d02690b6dd40bb3b45b109c4b7dd3ba3ff0e55a
AlexDesout <alexdesout@outlook.fr>
2023-04-12 16:01:52 +0200

    Bouton supprime

--- reactmusique/src/components/CommentaireCard.js
+++ reactmusique/src/components/CommentaireCard.js
@@ -6,6 +6,8 @@ import CardContent from '@mui/material/CardContent';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import { Link } from 'react-router-dom';
+import MonBouton from './MonBouton';
+import { UserContext } from '../App';
 
 const bull = (
     <Box
@@ -17,7 +19,9 @@ const bull = (
 );
 
 export default function CommentaireCard(data) {
+    const value = React.useContext(UserContext);
     // console.log(data);
+    if (value.admin == "1") {
     return (
         <Card className={'Commentaire'}>
             <CardContent>
@@ -35,7 +39,30 @@ export default function CommentaireCard(data) {
                 <Typography variant="body2">
                     {data.commentaire}
                 </Typography>
+                <MonBouton contenu = {"supprimer"}></MonBouton>
             </CardContent>
         </Card>
     );
+    }
+    else{
+        <Card className={'Commentaire'}>
+        <CardContent>
+            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                {data.note}/5
+            </Typography>
+            <Typography variant="h5" component="div">
+                <Link to={`/utilisateur/${data.pseudo}`}>
+                    <p>{data.pseudo}</p>
+                </Link>
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                {data.date}
+            </Typography>
+            <Typography variant="body2">
+                {data.commentaire}
+            </Typography>
+            
+        </CardContent>
+    </Card> 
+    }
 }
