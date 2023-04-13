ca9db15a53d947e3f738ec0b726ee6f38c44c844
Jules-Pelissou <jules.pelissou@gmail.com>
2023-04-13 10:07:00 +0200

    Ajout Form Connexion

--- musiquenative/components/FormConnexion.js
+++ musiquenative/components/FormConnexion.js
@@ -1,112 +1,72 @@
-import {useState, useEffect} from 'react';
-import { View, Text } from 'react-native'
-
-import { Link } from "react-router-dom";
-
-import Utilisateurs from '../../classes/Utilisateurs'
-import { UserContext } from "../../App"
-import { View } from "react-native/types";
-// import { userAdmin } from "../../App"
+import * as React from "react";
+import { View, Text, TextInput, Button } from "react-native";
+import { useState } from "react";
+//  import { userAdmin } from "../../App"
 
 
 
 
-export default function FormConnexion() {
- 
-// const navigation = useNavigation();
+export default function FormConnexion(props) {
 
-// let handlePress = () =>{
-//     navigation.navigate('Album', {
-//         id: data.id,
-//         titre: data.titre,
-//         cover: data.cover,
-//     });
-// }
-  
-//   const [pseudo, setPseudo] = useState("");
-//   const [mdp, setMdp] = useState("");
+   const [pseudo, setPseudo] = useState("");
+   const [mdp, setMdp] = useState("");
 
 
-//   const HandleChangePseudo = (event) => setPseudo(event.target.value);
-//   const HandleChangeMdp = (event) => setMdp(event.target.value);
+   const HandleChangePseudo = (event) => setPseudo(event.target.value);
+   const HandleChangeMdp = (event) => setMdp(event.target.value);
 
 
 
-//   const handleSubmit = (event) => {
-//     event.preventDefault();
-//     let data = new Utilisateurs(pseudo,null, mdp);
-//     props.handler(data);
-//     // console.log(data);
-//   }
+   const handleSubmit = (event) => {
+     event.preventDefault();
+     let data = new Utilisateurs(pseudo,null, mdp);
+     props.handler(data);
+      console.log(data);
+   }
   
 
 
 
 
   return (
-      <View>
-          <Text>Bonjour</Text>
-    {/* <Container component="main" maxWidth="xs">
-      <Box
-        sx={{
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center"
-        }}
-      >
-        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Connexion
-        </Typography>
-        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                id="pseudo"
-                label="Pseudo"
-                name="pseudo"
-                autoComplete="family-name"
-                value={pseudo}
-                onChange={HandleChangePseudo}
-                onChangeText={setPseudo}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                name="password"
-                label="Mot de passe"
-                type="password"
-                id="password"
-                autoComplete="new-password"
-                value={mdp}
-                onChange={HandleChangeMdp}
-                onChangeText={setMdp}
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Valider
-          </Button>
-          <Grid container justifyContent="flex-end">
-            <Link to="/inscription">
-              <Grid item>Vous n'avez pas de compte ? Inscription</Grid>
-            </Link>
-          </Grid>
-        </Box>
-      </Box>
-    </Container> */}
+    <View>
+    
+         <Text>
+           Connexion
+         </Text>
+               <TextInput
+                 required
+                 fullWidth
+                 id="pseudo"
+                 label="Pseudo"
+                 name="pseudo"
+                 autoComplete="family-name"
+                 value={pseudo}
+                 onChange={HandleChangePseudo}
+                 onChangeText={setPseudo}
+               />
+               <TextInput
+                 required
+                 fullWidth
+                 name="password"
+                 label="Mot de passe"
+                 type="password"
+                 id="password"
+                 autoComplete="new-password"
+                 value={mdp}
+                 onChange={HandleChangeMdp}
+                 onChangeText={setMdp}
+               />
+           <Button
+             type="submit"
+             fullWidth
+             variant="contained"
+             title="Valider"
+             sx={{ mt: 3, mb: 2 }}
+             onClick={handleSubmit}
+           >
+           </Button>
+            <Text>Vous n'avez pas de compte ? Inscription</Text>
     </View>
   );
 }
